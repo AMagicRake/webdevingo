6f4fbb97a4c9f214b073da3224b2310dc6e1a3e9
AMagicRake <niel.gow@live.co.uk>
2024-06-29 19:19:49 +0100

    Created First tempalte replace block

---
+++ 002_template/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type data struct {
+	Heading string
+}
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "index", data{"Test Replace"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}

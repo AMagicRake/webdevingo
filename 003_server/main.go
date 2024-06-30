be461ff7ff19c46efe0adf45fcd92814a98338f8
AMagicRake <niel.gow@live.co.uk>
2024-06-30 16:09:19 +0100

    added request/response handler

--- 003_server/main.go
+++ 003_server/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"strings"
 )
 
 func main() {
@@ -26,18 +26,67 @@ func main() {
 	}
 }
 
+// func handle(conn net.Conn) {
+// 	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+// 	if err != nil {
+// 		log.Println("CONN TIMEOUT")
+// 	}
+// 	scanner := bufio.NewScanner(conn)
+// 	for scanner.Scan() {
+// 		ln := scanner.Text()
+// 		fmt.Println(ln)
+// 		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+// 	}
+// 	defer conn.Close()
+
+// 	fmt.Println("Code got Here.")
+// }
+
 func handle(conn net.Conn) {
-	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
-	if err != nil {
-		log.Println("CONN TIMEOUT")
-	}
+	defer conn.Close()
+	request(conn)
+	response(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
 	scanner := bufio.NewScanner(conn)
+	hdr := make(map[string]string)
 	for scanner.Scan() {
 		ln := scanner.Text()
-		fmt.Println(ln)
-		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+		// fmt.Println(ln)
+		if i == 0 {
+			m := strings.Fields(ln)[0]
+			fmt.Println("***METHOD", m)
+		}
+		if i > 0 && ln != "" {
+			hdrSplit := strings.Split(ln, ":")
+			hdr[hdrSplit[0]] = hdrSplit[1]
+		}
+		if ln == "" {
+			//headers are done break
+			for key, val := range hdr {
+				fmt.Println(key, " : ", val)
+			}
+			break
+		}
+		i++
 	}
-	defer conn.Close()
+}
 
-	fmt.Println("Code got Here.")
+func response(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+</head>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }

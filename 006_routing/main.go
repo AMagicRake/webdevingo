18cda9d23c0e8a9936dfbd6a10dc521ec43440e9
AMagicRake <niel.gow@live.co.uk>
2024-07-01 14:51:47 +0100

    experimenting with mux

--- 006_routing/main.go
+++ 006_routing/main.go
@@ -5,21 +5,28 @@ import (
 	"net/http"
 )
 
-type hotdog int
+type dog int
+type cat int
+type index int
 
-func (hotdog) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/dog":
-		io.WriteString(res, "doggy")
-	case "/cat":
-		io.WriteString(res, "kitty")
-	default:
-		io.WriteString(res, "Hello!")
+func (dog) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "doggy")
+}
 
-	}
+func (cat) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "kitty")
+}
+func (index) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "Hello!")
 }
 
 func main() {
-	var d hotdog
-	http.ListenAndServe(":8080", d)
+	var d dog
+	var c cat
+	var i index
+	mux := http.NewServeMux()
+	mux.Handle("/", i)
+	mux.Handle("/dog", d)
+	mux.Handle("/cat", c)
+	http.ListenAndServe(":8080", mux)
 }

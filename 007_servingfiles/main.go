f14520835955de7146afe5acb7d709fc114f3ba4
AMagicRake <niel.gow@live.co.uk>
2024-07-02 14:05:51 +0100

    switched to serving an img folder

--- 007_servingfiles/main.go
+++ 007_servingfiles/main.go
@@ -8,17 +8,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("/shocked_pikachu.jpg", shockedPikachu)
+	// http.HandleFunc("/shocked_pikachu.jpg", shockedPikachu)
+	http.Handle("/img/", http.StripPrefix("/img", http.FileServer(http.Dir("img"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="/shocked_pikachu.jpg">`)
+	io.WriteString(w, `<img src="/img/shocked_pikachu.jpg">`)
 }
 
 func shockedPikachu(w http.ResponseWriter, req *http.Request) {
-	f, err := os.Open("shocked_pikachu.jpg")
+	f, err := os.Open("img/shocked_pikachu.jpg")
 	if err != nil {
 		http.Error(w, "File not Found", 404)
 	}

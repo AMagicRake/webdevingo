bb7ee9a76a212206397dfb294e195004fb51aceb
AMagicRake <niel.gow@live.co.uk>
2024-07-02 13:48:04 +0100

    Playing with Server Image

--- 007_servingfiles/main.go
+++ 007_servingfiles/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/shocked_pikachu.jpg", shockedPikachu)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/shocked_pikachu.jpg">`)
+}
+
+func shockedPikachu(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("shocked_pikachu.jpg")
+	if err != nil {
+		http.Error(w, "File not Found", 404)
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		http.Error(w, "File not Found", 404)
+	}
+
+	http.ServeContent(w, req, f.Name(), fi.ModTime(), f)
 }

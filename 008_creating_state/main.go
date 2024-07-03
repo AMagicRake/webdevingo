62a808e9f07de29892e065f5563de899686d012b
AMagicRake <niel.gow@live.co.uk>
2024-07-03 16:12:18 +0100

    testing form passing

--- 008_creating_state/main.go
+++ 008_creating_state/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 )
 
+var tpl *template.Template
+
 func main() {
+
+	tpl = template.Must(template.New("").ParseFiles("views.gotmpl"))
+
 	http.HandleFunc("/", urlParam)
+	http.HandleFunc("/form", formParam)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -15,4 +22,9 @@ func main() {
 func urlParam(w http.ResponseWriter, req *http.Request) {
 	v := req.FormValue("q")
 	io.WriteString(w, "Hello!, "+v)
+}
+
+func formParam(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl.ExecuteTemplate(w, "formOut", req.FormValue("q"))
 }

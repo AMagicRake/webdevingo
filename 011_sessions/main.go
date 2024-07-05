cfebfd8d38f2b164fd6b6b317aaaa87928e806b9
AMagicRake <niel.gow@live.co.uk>
2024-07-05 11:40:13 +0100

    created session maps

--- 011_sessions/main.go
+++ 011_sessions/main.go
@@ -2,31 +2,68 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"net/http"
 
 	"github.com/google/uuid"
 )
 
-func main() {
+type user struct {
+	UserName string
+	First    string
+	Last     string
+}
 
+var tpl *template.Template
+var dbUsers = map[string]user{}
+var dbSessions = map[string]string{}
+
+func main() {
+	tpl = template.Must(template.ParseFiles("views.gotmpl"))
 	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/bar", bar)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexPage(w http.ResponseWriter, req *http.Request) {
 
-	sessionId := uuid.New()
 	c, err := req.Cookie("sessionID")
 	if err != nil {
+		sessionId := uuid.New()
 		newCookie := &http.Cookie{Name: "sessionID", Value: sessionId.String()}
 		fmt.Println(newCookie)
 		http.SetCookie(w, newCookie)
-		io.WriteString(w, "Hi! Nice to meet you!\n")
-	} else {
-		io.WriteString(w, "Welcome Back, "+c.Value+"!\n")
 	}
-	io.WriteString(w, "I am the cookie monster!\n")
 
+	var u user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+		u = user{un, f, l}
+		dbSessions[c.Value] = un
+		dbUsers[un] = u
+	}
+
+	tpl.ExecuteTemplate(w, "index", u)
+
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("sessionID")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+	}
+
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+	}
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar", u)
 }

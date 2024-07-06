22a0346d198e506ae4534bc3c706f782618de4c2
AMagicRake <niel.gow@live.co.uk>
2024-07-06 12:56:49 +0100

    created db connection

---
+++ 013_mysql/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	//user:password@tcp(db:port)/dbName?charset=utf8
+	dbUser := os.Args[1]
+	dbPass := os.Args[2]
+	dbHost := os.Args[3]
+	dbPort := os.Args[4]
+	dbName := os.Args[5]
+	connString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbUser, dbPass, dbHost, dbPort, dbName)
+	fmt.Println(connString)
+	conn, err := sql.Open("mysql", connString)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conn.Close()
+}

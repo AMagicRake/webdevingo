ee23f81b62f5107aa9af497a24569b032c531767
AMagicRake <niel.gow@live.co.uk>
2024-07-06 12:57:30 +0100

    reformatting

--- 013_mysql/main.go
+++ 013_mysql/main.go
@@ -11,12 +11,12 @@ import (
 
 func main() {
 
-	//user:password@tcp(db:port)/dbName?charset=utf8
 	dbUser := os.Args[1]
 	dbPass := os.Args[2]
 	dbHost := os.Args[3]
 	dbPort := os.Args[4]
 	dbName := os.Args[5]
+	//user:password@tcp(db:port)/dbName?charset=utf8
 	connString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbUser, dbPass, dbHost, dbPort, dbName)
 	fmt.Println(connString)
 	conn, err := sql.Open("mysql", connString)
